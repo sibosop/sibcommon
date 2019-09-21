dfc1cf1049c3e7ad20412f2c56dbd733476d6c97
brian reinbolt <brian@eastshore.com>
2019-09-20 20:25:40 -0700

    fix fucking upgrade

--- upgrade.py
+++ upgrade.py
@@ -10,21 +10,10 @@ def upgrade():
     cmd = ['git','pull','origin','master']
     os.chdir("sibcommon")
     output = check_output(cmd)
-<<<<<<< HEAD
     cmd = ['git','-C','sibcommon','pull','origin','master']
     print("upgrade: %s"%(cmd))
     output = check_output(cmd)
     cmd = ['git','-C','speclib','pull','origin','master']
-=======
->>>>>>> 3bf942522f18b57d2be9038c6922deea87c06541
-    print("upgrade: %s"%(cmd))
-    os.chdir("../speclib")
-    output = check_output(cmd)
-<<<<<<< HEAD
-=======
-    print("upgrade: %s"%output)
-    os.chdir("..")
->>>>>>> 3bf942522f18b57d2be9038c6922deea87c06541
   except Exception, e:
     print("upgrade error: "+repr(e))
 
