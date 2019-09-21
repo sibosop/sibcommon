78b4364f210181e15abcc45be153c5af475d6543
brian reinbolt <brian@eastshore.com>
2019-09-20 20:28:49 -0700

    fix FUCKING GOD DAMN UPGRADE

--- upgrade.py
+++ upgrade.py
@@ -13,6 +13,7 @@ def upgrade():
     print("upgrade: %s"%(cmd))
     output = check_output(cmd)
     cmd = ['git','-C','speclib','pull','origin','master']
+    output = check_output(cmd)
   except Exception, e:
     print("upgrade error: "+repr(e))
 
